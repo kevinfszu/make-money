a58581d05c78b984af1515a4716a58763cfdff75
CullenFeng <410789180@qq.com>
2022-05-02 12:11:39 +0800

    nothing

--- TokenPriceMonitoring/test.js
+++ TokenPriceMonitoring/test.js
@@ -1,24 +1,35 @@
 const axios = require('axios')
+// const fetch = require('fetch')
+// const fetch = require('node-fetch')
 
 async function makeRequest() {
     const params = {
         symbol: 'ETH:DAI',
     }
     let url = `https://api.stg.deversifi.com/market-data/ticker/${params.symbol}`
-    const response = await axios(url, {
-        proxy: {
-            protocol: 'http',
-            host: '127.0.0.1',
-            port: 58591
-        },
-        method: 'get',
+    // const response = await axios({
+    //     proxy: {
+    //         protocol: 'socks5',
+    //         host: '127.0.0.1',
+    //         port: 51837
+    //     },
+    //     url: url,
+    //     method: 'get',
+    //     headers: {
+    //         'Accept': 'application/json',
+    //         'Content-Type': 'application/json'
+    //     }
+    // })
+
+    const response = await fetch(url, {
+        method: "GET",
         headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        }
+            "Accept": "application/json",
+            "Content-Type": "application/json",
+        },
     })
 
-    // console.log(response.toJSON())
+    console.log(response)
     // return response.json()
 }
 
